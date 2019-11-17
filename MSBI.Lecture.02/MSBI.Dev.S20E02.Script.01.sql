1233a667e366ecfca9a52bf4fa9a712a029cf9c4
Vladimir Mitiurin <Vladimir_Mitiurin@epam.com>
2019-11-17 21:52:47 +0400

    S20E02

--- MSBI.Lecture.02/MSBI.Dev.S20E02.Script.01.sql
+++ MSBI.Lecture.02/MSBI.Dev.S20E02.Script.01.sql
@@ -1,7 +1,9 @@
 /*=========================================
-MSBI.Dev.S19E02.Script.01.sql
-updated 20190204
+MSBI.Dev.S20E02.Script.01.sql
 =========================================*/
+
+USE [TSQL2012];
+GO
 
 --predicate
 SELECT [empid]
@@ -9,6 +11,7 @@ SELECT [empid]
       ,[region]
   FROM [TSQL2012].[HR].[Employees]
   WHERE [region]=N'WA'
+
 --Null
 SELECT [empid]
       ,[lastname]
@@ -19,7 +22,6 @@ SELECT [empid]
 
 --Using T-SQL in a Relational Way
 
-USE TSQL2012;
 SELECT country
 FROM HR.Employees;
 
@@ -31,40 +33,53 @@ FROM HR.Employees;
 
 -- order in sets
 -- result is relational
-SELECT empid, lastname
+SELECT
+	empid,
+	lastname
 FROM HR.Employees;
 
 -- let's make it ordered
 --the result isnt relational
-SELECT empid, lastname
+SELECT
+	empid,
+	lastname
 FROM HR.Employees
 ORDER BY empid;
 
-
 -- wrong way to sort
-
-SELECT empid, lastname
+SELECT
+	empid,
+	lastname
 FROM HR.Employees
 ORDER BY 1;
 
 
+--Aliases
+
 --wrong way
-SELECT empid, firstname + ' ' + lastname fullname
+SELECT
+	empid,
+	firstname + ' ' + lastname
+FROM HR.Employees;
+
+--wrong way
+SELECT
+	empid,
+	firstname + ' ' + lastname fullname
 FROM HR.Employees;
 
 --right way
-SELECT empid, firstname + ' ' + lastname AS fullname
+SELECT
+	empid,
+	firstname + ' ' + lastname AS fullname
 FROM HR.Employees;
-
 
 --right way
-SELECT empid, firstname + ' ' + lastname AS fullname
+SELECT
+	empid,
+	fullname =	firstname + ' ' + lastname
 FROM HR.Employees;
 
--- declarative
-
-SELECT shipperid, phone, companyname
-FROM Sales.Shippers;
 
 
 
@@ -74,26 +89,30 @@ Logical Query Processing Phases
 ================================================================================*/
 
 SELECT	country,
-           YEAR(hiredate) AS yearhired,
-           COUNT(*) AS numemployees
+        YEAR(hiredate) AS yearhired,
+        COUNT(*) AS numemployees
 FROM HR.Employees
      WHERE hiredate >= '20030101'
      GROUP BY country, YEAR(hiredate)
      HAVING COUNT(*) > 1
-     ORDER BY country , yearhired DESC;
+     ORDER BY COUNT(*) DESC;
 
 
 	--1 . Evaluate the FROM Clause
 
 
 	--typical mistake
-SELECT country, YEAR(hiredate) AS yearhired
+SELECT
+	country,
+	YEAR(hiredate) AS yearhired
 FROM HR.Employees
 WHERE yearhired >= 2003;
 
 
 --NON SARGABLE BUT WORKS
-SELECT country, YEAR(hiredate) AS yearhired
+SELECT
+	country,
+	YEAR(hiredate) AS yearhired
 FROM HR.Employees
 WHERE YEAR(hiredate) >= 2003;
 --should use YEAR(hiredate) >= 2003 better hiredate >= '20030101'
@@ -101,12 +120,25 @@ WHERE YEAR(hiredate) >= 2003;
 
 --Note that an alias created by the SELECT phase isnt even visible to other expressions that
 --appear in the same SELECT list. For example, the following query isnt valid.
---!!!!!!!  TODO Meaning????? 
--- Andrey to review
-SELECT empid, country, YEAR(hiredate) AS yearhired, YEAR(hiredate) - 1 AS prevyear
+SELECT
+	empid,
+	YEAR(hiredate) AS yearhired,
+	yearhired - 1 AS prevyear
 FROM HR.Employees;
 
+--Correct
+SELECT
+	empid,
+	YEAR(hiredate) AS yearhired,
+	YEAR(hiredate) - 1 AS prevyear
+FROM HR.Employees;
 
+--The only option
+SELECT
+	country,
+	YEAR(hiredate) AS yearhired
+FROM HR.Employees
+ORDER BY yearhired
 
 /*===========================================================================
 Using the FROM and SELECT Clauses
@@ -131,7 +163,7 @@ FROM HR.Employees;
 SELECT empid,  lastname = firstname
 FROM HR.Employees;
 
---redefime attribute name
+--redefine attribute name
 SELECT empid AS employeeid, firstname, lastname
 FROM HR.Employees;
 
@@ -149,5 +181,83 @@ SELECT 10 AS col1, 'ABC' AS col2;
 --=========================================================================
 -- Data Types
 --=========================================================================
-DECLARE @f AS FLOAT = '29545428.022495';
+
+-- Does it works?
+DECLARE @X INT = '1';
+SELECT @X;
+
+
+DECLARE @Y INT = 'AAA';
+SELECT @Y;
+
+
+DECLARE @S1 VARCHAR(1) = 1;
+SELECT @S1;
+
+
+DECLARE @S2 VARCHAR(1) = 1111;
+SELECT @S2;
+
+
+DECLARE @S3 VARCHAR(2) = 'AAA';
+SELECT @S3;
+
+
+--DON'T EVER DO IT!!!
+SELECT
+	empid,
+	lastname
+FROM HR.Employees
+WHERE empid = '1';
+
+
+--Simple arithmetic
+SELECT 1 / 2;
+
+
+--Solution
+SELECT 1.0 / 2;
+
+
+--What's now?
+SELECT 1 / 2 + 0.5;
+
+
+--Replicate and len functions
+DECLARE @V VARCHAR(MAX) = REPLICATE('A', 8000);
+SELECT
+	@V,
+	LEN(@V)
+;
+
+--Concatenation of long strings
+SELECT
+	LEN(REPLICATE('A', 8000))		+	LEN(REPLICATE('B', 8000)),
+	LEN(	REPLICATE('A', 8000)	+	REPLICATE('B', 8000)	)
+;
+
+--Solution
+SELECT
+	LEN(	CAST('' AS VARCHAR(MAX))	+	REPLICATE('A', 8000)	+	REPLICATE('B', 8000)	)
+;
+
+
+--String comparison
+SELECT
+	CASE WHEN 'AAA       ' = 'AAA'
+		THEN 'True'
+		ELSE 'False'
+	END
+;
+
+SELECT
+	CASE WHEN '       ' = ''
+		THEN 'True'
+		ELSE 'False'
+	END
+;
+
+
+--Float
+DECLARE @f AS FLOAT = 29545428.022495;
 SELECT CAST(@f AS NUMERIC(28, 14)) AS value;
