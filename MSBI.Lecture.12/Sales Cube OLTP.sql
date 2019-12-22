7656d0844e51e67dee1ef3cfedd8021c08dab1bf
Vladimir Mitiurin <Vladimir_Mitiurin@epam.com>
2019-12-22 23:56:27 +0400

    S20E12

--- MSBI.Lecture.12/Sales Cube OLTP.sql
+++ MSBI.Lecture.12/Sales Cube OLTP.sql
@@ -17,24 +17,24 @@ AS
 SELECT 
 	SD.ProductID,
 	SD.SalesAmount,
-	CASE WHEN SD.SalesYear ='2005'
+	CASE WHEN SD.SalesYear = 2011
 		THEN SD.SalesAmount 
-	END AS Sales2005,
-	CASE WHEN SD.SalesYear ='2006'
+	END AS Sales2011,
+	CASE WHEN SD.SalesYear = 2012
 		THEN SD.SalesAmount 
-	END AS Sales2006,
-	CASE WHEN SD.SalesYear ='2007'
+	END AS Sales2012,
+	CASE WHEN SD.SalesYear = 2013
 		THEN SD.SalesAmount 
-	END AS Sales2007,
-	CASE WHEN SD.SalesYear ='2008'
+	END AS Sales2013,
+	CASE WHEN SD.SalesYear = 2014
 		THEN SD.SalesAmount 
-	END AS Sales2008,
-	CASE WHEN SD.SalesYear ='2009'
+	END AS Sales2014,
+	CASE WHEN SD.SalesYear = 2015
 		THEN SD.SalesAmount 
-	END AS Sales2009,
-		CASE WHEN SD.SalesYear ='2010'
-	THEN SD.SalesAmount 
-		END AS Sales2010
+	END AS Sales2015,
+	CASE WHEN SD.SalesYear = 2016
+		THEN SD.SalesAmount 
+	END AS Sales2016
 FROM SD
 ),
 Sales
@@ -42,24 +42,24 @@ AS
 (
 SELECT 
 	SI.ProductID,
-	SUM(SI.Sales2005) AS '2005',
-	SUM(SI.Sales2006) AS '2006',
-	SUM(SI.Sales2007) AS '2007',
-	SUM(SI.Sales2008) AS '2008',
-	SUM(SI.Sales2009) AS '2009',
-	SUM(SI.Sales2010) AS '2010',
+	SUM(SI.Sales2011) AS '2011',
+	SUM(SI.Sales2012) AS '2012',
+	SUM(SI.Sales2013) AS '2013',
+	SUM(SI.Sales2014) AS '2014',
+	SUM(SI.Sales2015) AS '2015',
+	SUM(SI.Sales2016) AS '2016',
 	SUM(SI.SalesAmount) AS Total
 FROM SI
 GROUP BY SI.ProductID
 )
 SELECT 
 	P.Name,
-	S.[2005],
-	S.[2006],
-	S.[2007],
-	S.[2008],
-	S.[2009],
-	S.[2010],
+	S.[2011],
+	S.[2012],
+	S.[2013],
+	S.[2014],
+	S.[2015],
+	S.[2016],
 	S.Total
 FROM Sales AS S
 	INNER JOIN
